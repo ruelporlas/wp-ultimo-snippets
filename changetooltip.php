781edeb8cfaaa7ee1fb04489e206cd065afa7a57
ruelporlas <52828902+ruelporlas@users.noreply.github.com>
2019-08-21 23:12:39 +0800

    Change tooltip on sign up fields

---
+++ changetooltip.php
@@ -0,0 +1,11 @@
+/**
+ * Change tooltip on sign up fields.
+ */
+
+add_filter('wu_signup_site_title_tooltip', function() {
+	  return "your site title tooltip here";
+ });
+
+add_filter('wu_signup_site_url_tooltip', function() {
+	  return "your site URL tooltip here";
+ });
